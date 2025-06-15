edafd5e8b5ca0af4ec21f0f8f10cf491fafbff55
s-mgh <moh3en.ghorani@gmail.com>
2025-06-15 18:35:20 +0330

    Add files via upload

---
+++ install_release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+BUILDDIR=build-auto
+PREBUILT_SERVER_URL=https://github.com/Genymobile/scrcpy/releases/download/v3.3/scrcpy-server-v3.3
+PREBUILT_SERVER_SHA256=351cb2edc7e4c2c75f09a7933fdabcf137be52e2602df154f24ec02db46e9e51
+
+echo "[scrcpy] Downloading prebuilt server..."
+wget "$PREBUILT_SERVER_URL" -O scrcpy-server
+echo "[scrcpy] Verifying prebuilt server..."
+echo "$PREBUILT_SERVER_SHA256  scrcpy-server" | sha256sum --check
+
+echo "[scrcpy] Building client..."
+rm -rf "$BUILDDIR"
+meson setup "$BUILDDIR" --buildtype=release --strip -Db_lto=true \
+    -Dprebuilt_server=scrcpy-server
+cd "$BUILDDIR"
+ninja
+
+echo "[scrcpy] Installing (sudo)..."
+sudo ninja install
